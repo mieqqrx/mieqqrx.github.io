0fb526cf84acbd4dba2693ff4aa63ccc16dbb981
mieqqrx <149629978+mieqqrx@users.noreply.github.com>
2024-11-07 23:10:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,12 @@
-let uScore = document.getElementById("u-score");
-let cScore = document.getElementById("c-score"); 
+ 
 let button = document.getElementById("button"); 
 let tryAgainButton = document.getElementById("try-again"); 
-let res = document.getElementById("res");
 let attempt = document.getElementById('attempt');
-let uPoints = 0;
-let cPoints = 0;
-let current = 0;
 const rounds = 3;
-
-let cards = [
-    {src: 'img/6_of_clubs.png', value: 6},
-    {src: 'img/6_of_diamonds.png', value: 6},
-    {src: 'img/6_of_hearts.png', value: 6},
-    {src: 'img/6_of_spades.png', value: 6},
-
-    {src: 'img/7_of_clubs.png', value: 7},
-    {src: 'img/7_of_diamonds.png', value: 7},
-    {src: 'img/7_of_hearts.png', value: 7},
-    {src: 'img/7_of_spades.png', value: 7},
-
-    {src: 'img/8_of_clubs.png', value: 8},
-    {src: 'img/8_of_diamonds.png', value: 8},
-    {src: 'img/8_of_hearts.png', value: 8},
-    {src: 'img/8_of_spades.png', value: 8},
-
-    {src: 'img/9_of_clubs.png', value: 9},
-    {src: 'img/9_of_diamonds.png', value: 9},
-    {src: 'img/9_of_hearts.png', value: 9},
-    {src: 'img/9_of_spades.png', value: 9},
-
-    {src: 'img/10_of_clubs.png', value: 10},
-    {src: 'img/10_of_diamonds.png', value: 10},
-    {src: 'img/10_of_hearts.png', value: 10},
-    {src: 'img/10_of_spades.png', value: 10},
-
-    {src: 'img/jack_of_clubs.png', value: 2},
-    {src: 'img/jack_of_diamonds.png', value: 2},
-    {src: 'img/jack_of_hearts.png', value: 2},
-    {src: 'img/jack_of_spades.png', value: 2},
-
-    {src: 'img/queen_of_clubs.png', value: 3},
-    {src: 'img/queen_of_diamonds.png', value: 3},
-    {src: 'img/queen_of_hearts.png', value: 3},
-    {src: 'img/queen_of_spades.png', value: 3},
-
-    {src: 'img/king_of_clubs.png', value: 4},
-    {src: 'img/king_of_diamonds.png', value: 4},
-    {src: 'img/king_of_hearts.png', value: 4},
-    {src: 'img/king_of_spades.png', value: 4},
-
-    {src: 'img/ace_of_clubs.png', value: 11},
-    {src: 'img/ace_of_diamonds.png', value: 11},
-    {src: 'img/ace_of_hearts.png', value: 11},
-    {src: 'img/ace_of_spades.png', value: 11},
-];
+let res = document.getElementById('res');
+const icnH = 106, icnC = 9, icnArr = [0, 0, 0], icnT = 60;
+let current = 0;
+let slots = document.querySelector(".lines");
 
 window.onload = () => {
     let usname = prompt('enter your name:');
@@ -66,52 +18,74 @@ window.onload = () => {
     }
 };
 
-function randCard() {
-    let randIndex = Math.floor(Math.random() * cards.length);
-    return cards[randIndex];
-}
+let spin = (line, offset = 0) => {
+    let random = (offset + 2) * icnC + Math.floor(Math.random() * icnC);
+    let bgPosY = parseInt(window.getComputedStyle(line).backgroundPositionY) || 0;
+
+    let targetBgPos = bgPosY + random * icnH;
+    let normPos = targetBgPos%(icnH*icnC);
+
+    return new Promise((resolve) => {
+        line.style.transition = `${8 + random * icnT}ms`;
+        line.style.backgroundPositionY = `${targetBgPos}px`;   
+        setTimeout(() => {
+            line.style.transition = "none";
+            line.style.backgroundPositionY = `${normPos}px`; 
+            resolve(random%icnC);     
+        }, 8 + random * icnT); 
+    });
+};
 
 button.addEventListener('click', () => {
-    if (current < rounds) {
-        let uCard = randCard();
-        let cCard = randCard();
-
-        uPoints += uCard.value;
-        cPoints += cCard.value;
-
-        uScore.innerHTML = uPoints;
-        cScore.innerHTML = cPoints;
-        document.getElementById('u-card').src = uCard.src;
-        document.getElementById('c-card').src = cCard.src;
-
-        current++;
-        attempt.style.display = 'block';
-        attempt.innerHTML = `attempt ${current} of ${rounds}`;
-
-        if (current == rounds) {
-            res.style.display = 'block';
-            button.style.display = 'none';
-            tryAgainButton.style.display = 'block';
-
-            if (uPoints > cPoints) {
-                res.innerHTML = 'You win!';
-            } else if (uPoints < cPoints) {
-                res.innerHTML = 'You lose!';
-            }
+    let lines = document.querySelectorAll(".line");
+    Promise
+    .all([...lines].map((line, index) => spin(line, index)))
+    .then((random) => {
+        random.forEach((random, index) => {
+            icnArr[index] = (icnArr[index] + random)%icnC;
+        });
+        if(icnArr[0] == icnArr[1] && icnArr[1] == icnArr[2]){
+            res.innerHTML = 'You win!';
+            res.style.display = "block";
+            slots.classList.add("win");
+            setTimeout(() => {
+                slots.classList.remove("win")
+            }, 3000);
         }
+        if(current == 3){
+            res.innerHTML = 'You lose!';
+            res.style.display = "block";
+            slots.classList.add("lose");
+            setTimeout(() => {
+                slots.classList.remove("lose")
+            }, 3000);
+        }
+    });
+    current++;
+    attempt.style.display = 'block';
+    attempt.innerHTML = `attempt ${current} of ${rounds}`;
+    if (current == rounds) {
+        res.style.display = 'block';
+        button.style.display = 'none';
+        tryAgainButton.style.display = 'block';
     }
 });
 
 tryAgainButton.addEventListener('click', () => {
     button.style.display = 'block';
     tryAgainButton.style.display = 'none';
-    uPoints = 0;
-    cPoints = 0;
     current = 0;
-    uScore.innerHTML = '0';
-    cScore.innerHTML = '0';
-    document.getElementById('u-card').src = 'img/card.png';
-    document.getElementById('c-card').src = 'img/card.png';
     attempt.style.display = 'none';
     res.innerHTML = '';
+    icnArr.fill(0);
+    let lines = document.querySelectorAll(".line");
+    lines.forEach((element) => {
+        element.style.backgroundPositionY = 0;
+        element.style.transition = '1s ease';
+        
+    })
 });
+
+
+
+
